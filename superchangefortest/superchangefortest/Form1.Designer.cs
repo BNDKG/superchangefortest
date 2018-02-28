82e8d3fa152be8895b35f8bfbe9c31d8ae79311a
zhangmingchen <zhangmingchen@ZMCLAPTOP>
2018-02-28 16:18:59 +0800

    首次提交，用于LED测试

---
+++ superchangefortest/superchangefortest/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace superchangefortest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new AForge.Controls.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new AForge.Controls.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 16);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(316, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(316, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = null;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 16);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(358, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 49);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(150, 223);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 29);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(150, 298);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 29);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(82, 261);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 29);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(220, 261);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 29);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 371);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private AForge.Controls.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ superchangefortest/superchangefortest/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace superchangefortest
+{
+    public partial class Form1 : Form
+    {
+
+        System.Drawing.Image img1;
+        System.Drawing.Image img2;
+        System.Drawing.Image img3;
+        System.Drawing.Image img4;
+        System.Drawing.Image img5;
+        System.Drawing.Image img6;
+        System.Drawing.Image img7;
+        System.Drawing.Image img8;
+        System.Drawing.Image img9;
+        System.Drawing.Image img10;
+        System.Drawing.Image img11;
+        System.Drawing.Image img12;
+        System.Drawing.Image img13;
+        System.Drawing.Image img14;
+        System.Drawing.Image img15;
+
+        int flag = 1;
+        int flag2 = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            img1 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged1.jpg");//双引号里是图片的路径
+            img2 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged2.jpg");//双引号里是图片的路径
+            img3 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged3.jpg");//双引号里是图片的路径
+            img4 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged4.jpg");//双引号里是图片的路径
+            img5 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged5.jpg");//双引号里是图片的路径
+            img6 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged6.jpg");//双引号里是图片的路径
+            img7 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged7.jpg");//双引号里是图片的路径
+            img8 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged8.jpg");//双引号里是图片的路径
+            img9 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged9.jpg");//双引号里是图片的路径
+            img10 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged10.jpg");//双引号里是图片的路径
+            img11 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged11.jpg");//双引号里是图片的路径
+            img12 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged12.jpg");//双引号里是图片的路径
+            img13 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged13.jpg");//双引号里是图片的路径
+            img14 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged14.jpg");//双引号里是图片的路径
+            img15 = System.Drawing.Image.FromFile("C:\\Users\\zhangmingchen\\Desktop\\timgchanged15.jpg");//双引号里是图片的路径
+            timer1.Start();
+
+
+
+            //System.Threading.Thread.Sleep(1000);//
+
+            //pictureBox1.Image = img2;
+            //pictureBox2.Image = img;
+
+            //System.Threading.Thread.Sleep(1000);//
+
+
+            /*
+            int i = 0;
+            while (true)
+            {
+                if (i == 0)
+                {
+                    pictureBox1.Image = img;
+                    pictureBox2.Image = img2;
+                    i = 1;
+                }
+                else
+                {
+
+                    pictureBox1.Image = img2;
+                    pictureBox2.Image = img;
+
+                    i = 0;
+                }
+
+
+            }
+            */
+
+        }
+        public struct Pixel
+        {
+            public byte B;
+            public byte G;
+            public byte R;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            Bitmap b = new Bitmap("C:\\Users\\zhangmingchen\\Desktop\\timg1.jpg");
+
+            Pixel[,] changed_data = new Pixel[b.Width, b.Height];
+
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite,
+            PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;   // 扫描的宽度 
+
+            unsafe
+            {
+                byte* p = (byte*)bmData.Scan0.ToPointer(); // 获取图像首地址 
+                byte* p2 = null;
+                int nOffset = stride - b.Width * 3;  // 实际宽度与系统宽度的距离 
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+
+                        changed_data[x, y].R= p[0];
+                        changed_data[x, y].G = p[1];
+                        changed_data[x, y].B = p[2];
+
+                        p += 3;  // 跳过3个字节处理下个像素点 
+                    }
+                    p += nOffset; // 加上间隔 
+                }
+            }
+            b.UnlockBits(bmData); // 解锁 
+
+
+
+            Bitmap bitmap_temp3 = zzz2(changed_data);
+            bitmap_temp3.Save("C:\\Users\\zhangmingchen\\Desktop\\timgchanged1.jpg");
+
+
+        }
+        Bitmap zzz2(Pixel[,] cuted_data)
+        {
+            Bitmap b = new Bitmap(cuted_data.GetLength(0), cuted_data.GetLength(1)/3);
+
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, (b.Height)), ImageLockMode.ReadWrite,
+            PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;   // 扫描的宽度 
+
+            unsafe
+            {
+                byte* p = (byte*)bmData.Scan0.ToPointer(); // 获取图像首地址 
+                byte* p2 = null;
+                int nOffset = stride - b.Width * 3;  // 实际宽度与系统宽度的距离 
+
+                for (int y = 0; y < (b.Height); ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+                        p[2] = (byte)rgbchange(cuted_data,x,y);
+                        p[1]= (byte)rgbchange(cuted_data, x, b.Height+y);
+                        p[3]= (byte)rgbchange(cuted_data, x, b.Height*2+y);
+
+                        p += 3;  // 跳过3个字节处理下个像素点 
+                    }
+                    p += nOffset; // 加上间隔 
+                }
+            }
+            b.UnlockBits(bmData); // 解锁 
+
+            return b;
+        }
+        int rgbchange(Pixel[,] cuted_data,int x,int y)
+        {
+            int final = (cuted_data[x, y].R + cuted_data[x, y].G + cuted_data[x, y].B) / 3;
+
+            if (final>100)
+            {
+                final = 255;
+            }
+            else
+            {
+                final = 0;
+            }
+            return final;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(0,-10);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Location = new Point(124, 638);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap("C:\\Users\\zhangmingchen\\Desktop\\timg2.jpg");
+
+            Pixel[,] changed_data = new Pixel[b.Width, b.Height];
+
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite,
+            PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;   // 扫描的宽度 
+
+            unsafe
+            {
+                byte* p = (byte*)bmData.Scan0.ToPointer(); // 获取图像首地址 
+                byte* p2 = null;
+                int nOffset = stride - b.Width * 3;  // 实际宽度与系统宽度的距离 
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+
+                        changed_data[x, y].R = p[0];
+                        changed_data[x, y].G = p[1];
+                        changed_data[x, y].B = p[2];
+
+                        p += 3;  // 跳过3个字节处理下个像素点 
+                    }
+                    p += nOffset; // 加上间隔 
+                }
+            }
+            b.UnlockBits(bmData); // 解锁 
+
+
+
+            Bitmap bitmap_temp3 = zzz2(changed_data);
+            bitmap_temp3.Save("C:\\Users\\zhangmingchen\\Desktop\\timgchanged2.jpg");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            if (flag == 1) {
+                pictureBox1.Image = img1;
+                flag++;
+            }
+            else if(flag == 2)
+            {
+                pictureBox1.Image = img2;
+                flag++;
+            }
+            else if (flag == 3)
+            {
+                pictureBox1.Image = img3;
+                flag++;
+            }
+            else if (flag == 4)
+            {
+                pictureBox1.Image = img4;
+                flag++;
+            }
+            else if (flag == 5)
+            {
+                pictureBox1.Image = img5;
+                
+                flag++;
+            }
+            else if (flag == 6)
+            {
+                pictureBox1.Image = img6;
+               
+                flag++;
+            }
+            else if (flag == 7)
+            {
+                pictureBox1.Image = img7;
+                flag++;
+            }
+            else if (flag == 8)
+            {
+                pictureBox1.Image = img8;
+                flag++;
+            }
+            else if (flag == 9)
+            {
+                pictureBox1.Image = img9;
+                flag++;
+            }
+            else if (flag == 10)
+            {
+                pictureBox1.Image = img10;
+                flag=1;
+            }
+
+            if (flag2 == 1)
+            {
+                pictureBox2.Image = img11;
+                flag2++;
+            }
+            else if (flag2 == 2)
+            {
+                pictureBox2.Image = img12;
+                flag2++;
+            }
+            else if (flag2 == 3)
+            {
+                pictureBox2.Image = img13;
+                flag2++;
+            }
+            else if (flag2 == 4)
+            {
+                pictureBox2.Image = img14;
+                flag2++;
+            }
+            else if (flag2 == 5)
+            {
+                pictureBox2.Image = img15;
+                flag2=1;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Point bufpoint = this.Location;
+            bufpoint.Y -= 10;
+
+            this.Location = bufpoint;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Point bufpoint = this.Location;
+            bufpoint.Y += 10;
+
+            this.Location = bufpoint;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Point bufpoint = this.Location;
+            bufpoint.X -= 10;
+
+            this.Location = bufpoint;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Point bufpoint = this.Location;
+            bufpoint.X += 10;
+
+            this.Location = bufpoint;
+        }
+    }
+}
